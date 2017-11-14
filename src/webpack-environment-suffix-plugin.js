6b8059fe946b34ab65db3a066a539351e0da711f
AksimO <mpomanenko@yahoo.com>
2017-11-14 15:42:06 +0100

    Rename options to include, exclude

--- src/webpack-environment-suffix-plugin.js
+++ src/webpack-environment-suffix-plugin.js
@@ -1,19 +1,17 @@
 /**
- * Returns function that checks path agains set of arrays
+ * Returns function that checks path against set of arrays
  * @param {RegExp[]} regexps
  * @return (path: string) => boolean
  */
 const regexps2function = regexps => path =>
-  Array.isArray(regexps)
-    ? regexps.some(regexp => regexp.test(path))
-    : regexps.test(path);
+  regexps.length && regexps.some(regexp => regexp.test(path));
 
 /**
- * Returns function that is used by reolver to search env dependent files 
- * @param {string} outputFormat 
- * @param {string} suffix 
- */
-const formart2function = (outputFormat, suffix) => {
+* Returns function that is used by resolver to search env dependent files 
+* @param {string} outputFormat 
+* @param {string} suffix 
+*/
+const format2function = (outputFormat, suffix) => {
   const fileRegexp = /(.+)[\\/]([^\\/]+)$/;
   return requestName => {
     const match = requestName.match(fileRegexp);
@@ -31,37 +29,34 @@ class EnvironmentSuffixPlugin {
   constructor({
     ext = 'js',
     suffix = process.env.NODE_ENV,
-    excludeRequest = [/^[^\.]/],
-    output = '[name].[suffix]',
-    test = new RegExp(`${suffix}\\.(${ext})$`),
-    excludeContext = [/node_modules/]
+    include = [new RegExp(`.+${ext}$`)],
+    exclude = [/node_modules/],
+    output = '[name].[suffix]'
   }) {
     this.ext = ext;
     this.suffix = suffix;
-    this.excludeRequest = prop2func(excludeRequest, () =>
-      regexps2function(excludeRequest)
-    );
-    this.excludeContext = prop2func(excludeContext, () =>
-      regexps2function(excludeContext)
-    );
+    this.include = prop2func(include, () => regexps2function(include));
+    this.exclude = prop2func(exclude, () => regexps2function(exclude));
     this.output = prop2func(output, () =>
-      formart2function(output, this.suffix, this.ext)
+      format2function(output, this.suffix, this.ext)
     );
   }
 
   checkRequest(fs, { request, context }) {
-    if (this.excludeContext(context) || this.excludeRequest(request)) {
+    const moduleName = fs.join(context, request);
+    const file = `${moduleName}.${this.ext}`;
+
+    if (!this.include(file) || this.exclude(file)) {
       return Promise.reject();
     }
 
     return new Promise((resolve, reject) => {
-      const file = fs.join(context, request);
-      const newFileName = this.output(file);
+      const outputModuleName = this.output(moduleName);
 
-      //readfile to check if it's <env> dependent version exist
+      //read file to check if it's <env> dependent version exist
       fs.fileSystem.readFile(
-        `${newFileName}.${this.ext}`,
-        (error, response) => (error ? reject() : resolve(newFileName))
+        `${outputModuleName}.${this.ext}`,
+        (error, response) => (error ? reject() : resolve(outputModuleName))
       );
     }).then(newModule => {
       console.log(`Resolve ${request} => ${newModule}`);
@@ -70,8 +65,8 @@ class EnvironmentSuffixPlugin {
   }
 
   /**
-   * This method should be defined in order to work as webpack plugin
-   */
+* This method should be defined in order to work as webpack plugin
+*/
   apply(compiler) {
     compiler.plugin('normal-module-factory', nmf => {
       console.log(`Resolve suffix ${this.suffix} for files *.${this.ext}`);
